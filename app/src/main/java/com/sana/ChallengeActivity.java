9c7aeb544db392e396e39e0fb7a5b1a4dd9d862e
Fanny Rifqi Al-Farizzi <farizfarhan64@gmail.com>
2019-08-27 01:23:22 +0700

    jo challenge fix database

--- app/src/main/java/com/sana/ChallengeActivity.java
+++ app/src/main/java/com/sana/ChallengeActivity.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class ChallengeActivity extends AppCompatActivity {
 
     //private final String JSON_URL = "https://gist.githubusercontent.com/aws1994/f583d54e5af8e56173492d3f60dd5ebf/raw/c7796ba51d5a0d37fc756cf0fd14e54434c547bc/anime.json" ;
-    private final String JSON_URL = "http://192.168.100.5/php/index.json";
+    private final String JSON_URL = "https://lanuginose-numbers.000webhostapp.com/";
     private JsonArrayRequest request ;
     private RequestQueue requestQueue ;
     private List<Model_Challenge> mListItem ;
