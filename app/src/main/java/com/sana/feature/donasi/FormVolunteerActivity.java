1e2ee0492c614365ed6ca7b1177530e0e4dc571b
Alfiansyah Nur Zufa <anurzufa@gmail.com>
2019-09-15 20:38:42 +0700

    konoyon - Form Volunteer done

---
+++ app/src/main/java/com/sana/feature/donasi/FormVolunteerActivity.java
@@ -0,0 +1,13 @@
+package com.sana.feature.donasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormVolunteerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_volunteer);
+    }
+}
