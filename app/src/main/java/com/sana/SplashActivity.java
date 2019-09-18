11ceeef8410334199efc89ae6f19edcb46d922b1
Fanny Rifqi Al-Farizzi <farizfarhan64@gmail.com>
2019-09-18 17:41:46 +0700

    jo

--- app/src/main/java/com/sana/SplashActivity.java
+++ app/src/main/java/com/sana/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.sana.activity;
+package com.sana;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +7,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
+import com.sana.OnBoardingActivity;
 import com.sana.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -24,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
         downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
         img_logo.setAnimation(mytransition);
         img_pohon.setAnimation(downtoup);
-        final Intent i = new Intent(this,OnBoardingActivity.class);
+        final Intent i = new Intent(this, OnBoardingActivity.class);
         Thread timer = new Thread(){
             public void run(){
                 try {
