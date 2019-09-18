c3e24739911f6be0a97537389d33dc40a4d2e5e3
Alfiansyah Nur Zufa <anurzufa@gmail.com>
2019-09-18 11:38:42 +0700

    Konoyon - Update Proses Donasi

---
+++ app/src/main/java/com/sana/feature/donasi/DonasiProsesActivity.java
@@ -0,0 +1,13 @@
+package com.sana.feature.donasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonasiProsesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donasi_proses);
+    }
+}
